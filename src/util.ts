17f00c06629e529131f838933152a68a5cdf43d2
holly.hao <hao.holly@gmail.com>
2020-07-06 11:16:01 +0800

    refactor codes

--- src/util.ts
+++ src/util.ts
@@ -4,8 +4,9 @@ import CryptoJS from 'crypto-js';
 import uuid from 'uuid';
 import {ParsedUrlQuery} from "querystring";
 
-const Content_Type_Form_Data = 'application/x-www-form-urlencoded';
-const Content_Type_Json_Data = 'application/json';
+const Content_Type_Form_Data = 'application/x-www-form-urlencoded; charset=utf-8';
+const Content_Type_Json_Data = 'application/octet-stream; charset=utf-8';
+const Accept_JSON = 'application/json; charset=utf-8';
 let seq = 1;
 
 function seqInc() {
@@ -34,7 +35,31 @@ class Util {
         this.stage = stage;
     }
 
-    buildStringToSign(method: string, headers: Header, signedHeadersStr: string, url: UrlWithParsedQuery, data: FormData={}) {
+    static buildUrl(parsedUrl: UrlWithParsedQuery, data = {}): string {
+        let toStringify = Object.assign(parsedUrl.query, data);
+        let result = parsedUrl.pathname as string;
+        if (Object.keys(toStringify).length) {
+            let list = this.buildParameters(toStringify);
+            result += '?' + list.join('&');
+        }
+        return result;
+    }
+
+    static buildParameters(toStringify: ParsedUrlQuery): string[] {
+        let keys = Object.keys(toStringify).sort();
+        let list = new Array(keys.length);
+        for (let i = 0; i < keys.length; i++) {
+            let key = keys[i];
+            if (toStringify[key] !== undefined && toStringify[key] !== null && ('' + toStringify[key])) {
+                list[i] = `${key}=${toStringify[key]}`;
+            } else {
+                list[i] = `${key}`;
+            }
+        }
+        return list;
+    }
+
+    buildStringToSign(method: string, headers: Header, signedHeadersStr: string, url: UrlWithParsedQuery, data: FormData = {}) {
         // accept, contentMD5, contentType,
         const lf = '\n';
         let list: string[] = [method, lf];
@@ -81,15 +106,15 @@ class Util {
         return CryptoJS.HmacSHA256(stringToSign, this.appSecret).toString(CryptoJS.enc.Base64);
     }
 
-    md5(content: string) {
+    static md5(content: string) {
         return CryptoJS.MD5(content).toString(CryptoJS.enc.Base64);
     }
 
     getSignHeaderKeys(headers: {}) {
-        var keys = Object.keys(headers).sort();
-        var signKeys = [];
-        for (var i = 0; i < keys.length; i++) {
-            var key = keys[i];
+        const keys = Object.keys(headers).sort();
+        const signKeys = [];
+        for (let i = 0; i < keys.length; i++) {
+            const key = keys[i];
             // x-ca- 开头的header或者指定的header
             if (key.startsWith('x-ca-')) {
                 signKeys.push(key);
@@ -100,38 +125,14 @@ class Util {
         return signKeys.sort();
     }
 
-    static buildUrl(parsedUrl: UrlWithParsedQuery, data = {}): string {
-        let toStringify = Object.assign(parsedUrl.query, data);
-        let result = parsedUrl.pathname as string;
-        if (Object.keys(toStringify).length) {
-            let list = this.buildParameters(toStringify);
-            result += '?' + list.join('&');
-        }
-        return result;
-    }
-
-    static buildParameters(toStringify: ParsedUrlQuery) {
-        let keys = Object.keys(toStringify).sort();
-        let list = new Array(keys.length);
-        for (let i = 0; i < keys.length; i++) {
-            let key = keys[i];
-            if (toStringify[key] !== undefined && toStringify[key] !== null && ('' + toStringify[key])) {
-                list[i] = `${key}=${toStringify[key]}`;
-            } else {
-                list[i] = `${key}`;
-            }
-        }
-        return list;
-    }
-
     buildHeaders(headers = {}, extra = {}): Header {
         return Object.assign({
-            'ca_version': '1',
-            'x-ca-timestamp': Date.now(),
-            'x-ca-key': this.appKey,
-            'x-ca-nonce': uuid.v4(),
-            'x-ca-seq': new Number(seqInc()).toString(),
-            'x-ca-stage': this.stage,
+            'ca_version': ['1'],
+            'x-ca-timestamp': [Date.now()],
+            'x-ca-key': [this.appKey],
+            'x-ca-nonce': [uuid.v4()],
+            'x-ca-seq': [Number(seqInc()).toString()],
+            'x-ca-stage': [this.stage],
         }, headers, extra);
     }
 
@@ -149,28 +150,28 @@ class Util {
     // normal post or get to ws signed request
     createSignedRequestHeaders(method: string, url: string, requestHeaders: Header, contentType: string, acceptType: string, webSocketApiType?: WebSocketApiType, body?: string | FormData, deviceId?: string) {
         // 小写化，合并之后的headers
-        let headers = this.buildHeaders(requestHeaders, {'content-type': contentType, 'accept': acceptType});
+        let headers = this.buildHeaders(requestHeaders, {'content-type': [contentType], 'accept': [acceptType]});
         if (webSocketApiType) {
-            headers['x-ca-websocket_api_type'] = webSocketApiType;
+            headers['x-ca-websocket_api_type'] = [webSocketApiType];
         }
         if (deviceId) {
-            headers['x-ca-deviceid'] = deviceId;
+            headers['x-ca-deviceid'] = [deviceId];
         }
         let requestContentType = headers['content-type'] || '';
         //stream form
         if (method === 'POST' && !requestContentType.startsWith(Content_Type_Form_Data) && body) {
-            headers['content-md5'] = this.md5(body as string || '');
+            headers['content-md5'] = [Util.md5(body as string)];
         }
 
         let signHeaderKeys = this.getSignHeaderKeys(headers);
-        headers['x-ca-signature-headers'] = signHeaderKeys.join(',');
+        headers['x-ca-signature-headers'] = [signHeaderKeys.join(',')];
         let signedHeadersStr = this.getSignedHeadersString(signHeaderKeys, headers);
 
         let parsedUrl = parse(url, true);
         let stringToSign = this.buildStringToSign(method, headers, signedHeadersStr, parsedUrl, body as FormData);
-        headers['x-ca-signature'] = this.sign(stringToSign);
+        headers['x-ca-signature'] = [this.sign(stringToSign)];
         return headers;
     }
 }
 
-export {Util, Stage, seqInc, WebSocketApiType, Content_Type_Form_Data, Content_Type_Json_Data, FormData}
+export {Util, Stage, seqInc, WebSocketApiType, Content_Type_Form_Data, Content_Type_Json_Data, Accept_JSON, FormData}
--- src/ws.ts
+++ src/ws.ts
@@ -1,5 +1,13 @@
 import uuid from 'uuid';
-import {Content_Type_Form_Data, Content_Type_Json_Data, seqInc, Util, WebSocketApiType, FormData} from './util';
+import {
+    Accept_JSON,
+    Content_Type_Form_Data,
+    Content_Type_Json_Data,
+    FormData,
+    seqInc,
+    Util,
+    WebSocketApiType
+} from './util';
 import url, {parse} from 'url';
 import Timeout = NodeJS.Timeout;
 
@@ -43,7 +51,7 @@ class WS {
         this.ws = null;
     }
 
-    register(update: EventListener, deviceId: string, bodyInJson?: string|FormData) {
+    register(update: EventListener, deviceId: string, bodyInJson?: string | FormData) {
         this.ws = new WebSocket(this.config.url);
         let that = this;
         this.ws.onopen = function open() {
@@ -127,53 +135,22 @@ class WS {
 
     send(method: string, path: string, webSocketApiType?: WebSocketApiType, body?: string | FormData): void {
         let data = '';
+        let contentType = Content_Type_Json_Data;
         //if form data, else json
         if (body && body instanceof FormData) {
             data = this.formDataString(path, body);
+            contentType = Content_Type_Form_Data;
+        }
+        if (this.config.authType === 'none' || this.config.authType === 'appCode') {
+            const msg = this.createMsg(method, this.host, path, 'COMMON', contentType, Accept_JSON, data);
+            this.ws!.send(JSON.stringify(msg));
+            return msg;
         }
 
-        if (this.config.authType === 'none') {
-            const msg = {
-                method: method,
-                host: this.host,
-                headers: Object.assign({
-                    'x-ca-seq': new Number(seqInc()).toString(),
-                    'x-ca-nonce': uuid.v4().toString(),
-                    'date': new Date().toUTCString(),
-                    'x-ca-timestamp': new Date().getTime().toString(),
-                    'isBase64': 0,
-                    'ca_version': '1',
-                }, webSocketApiType ? {'x-ca-websocket_api_type': webSocketApiType} : {}),
-                path: path,
-                body: data || body || '',
-            };
-            this.ws!.send(JSON.stringify(msg));
-            return;
-        }
-
-        if (this.config.authType === 'appCode') {
-            const msg = {
-                method: method,
-                host: this.host,
-                headers: Object.assign({
-                    'x-ca-seq': new Number(seqInc()).toString(),
-                    'x-ca-nonce': uuid.v4().toString(),
-                    'date': new Date().toUTCString(),
-                    'x-ca-timestamp': new Date().getTime().toString(),
-                    'Authorization': `APPCODE ${this.config.appCode as string}`,
-                    'isBase64': 0,
-                    'ca_version': '1',
-                }, webSocketApiType ? {'x-ca-websocket_api_type': webSocketApiType} : {}),
-                path: path,
-                body: data || body || '',
-            };
-            this.ws!.send(JSON.stringify(msg));
-            return;
-        }
         // else signature mode
         const util = new Util(this.config.appKey as string, this.config.appSecret as string, this.config.stage);
         const headers = util.createSignedRequestHeaders('POST', new URL(path, this.config.url).toString(), {},
-            (body && body instanceof FormData) ? Content_Type_Form_Data : Content_Type_Json_Data, Content_Type_Json_Data, webSocketApiType, body)
+            contentType, Content_Type_Json_Data, webSocketApiType, body)
         const msg = {
             method: 'POST',
             host: this.host,
@@ -188,48 +165,17 @@ class WS {
 
     private regMsg(host: string, registerPath = '/register', body?: string | FormData) {
         let data = '';
+        let contentType = Content_Type_Json_Data;
         //if form data, else json
         if (body && body instanceof FormData) {
             data = this.formDataString(registerPath, body);
+            contentType = Content_Type_Form_Data;
         }
-        if (this.config.authType === 'none') {
-            const msg = {
-                method: 'POST',
-                host: host,
-                headers: {
-                    'x-ca-websocket_api_type': 'REGISTER',
-                    'x-ca-seq': new Number(seqInc()).toString(),
-                    'x-ca-nonce': uuid.v4().toString(),
-                    'date': new Date().toUTCString(),
-                    'x-ca-timestamp': new Date().getTime().toString(),
-                    'isBase64': 0,
-                    'ca_version': '1',
-                },
-                path: registerPath,
-                body: data || body || '',
-            };
+        if (this.config.authType === 'none' || this.config.authType === 'appCode') {
+            const msg = this.createMsg('POST', host, registerPath, 'REGISTER', contentType, Accept_JSON, data);
             return msg;
         }
 
-        if (this.config.authType === 'appCode') {
-            const msg = {
-                method: 'POST',
-                host: host,
-                headers: {
-                    'x-ca-websocket_api_type': 'REGISTER',
-                    'x-ca-seq': new Number(seqInc()).toString(),
-                    'x-ca-nonce': uuid.v4().toString(),
-                    'date': new Date().toUTCString(),
-                    'x-ca-timestamp': new Date().getTime().toString(),
-                    'Authorization': `APPCODE ${this.config.appCode as string}`,
-                    'isBase64': 0,
-                    'ca_version': '1',
-                },
-                path: registerPath,
-                body: data || body || '',
-            };
-            return msg;
-        }
         // else signature mode
         const util = new Util(this.config.appKey as string, this.config.appSecret as string, this.config.stage);
         const headers = util.createSignedRequestHeaders('POST', new URL(registerPath, this.config.url).toString(), {},
@@ -245,54 +191,56 @@ class WS {
         return msg;
     }
 
+    private createMsg(method: string, host: string, path: string, api_type = 'COMMON', content_type: string, accept: string, body?: string) {
+        const msg: any = {
+            method: method,
+            host: host,
+            headers: {
+                'content-type': [content_type],
+                'accept': [accept],
+                'x-ca-stage': [this.config.stage],
+                'x-ca-websocket_api_type': [api_type],
+                'x-ca-seq': [new Number(seqInc()).toString()],
+                'x-ca-nonce': [uuid.v4().toString()],
+                'date': [new Date().toUTCString()],
+                'x-ca-timestamp': [new Date().getTime().toString()],
+                'ca_version': ['1'],
+            },
+            path: path,
+            'isBase64': 0,
+
+        };
+
+        if (this.config.appCode) {
+            msg.headers['Authorization'] = [`APPCODE ${this.config.appCode as string}`];
+        }
+        if (body) {
+            msg.body = body;
+            msg.headers['content-md5'] = [Util.md5(body)];
+        }
+        return msg;
+    }
+
     private unregMsg(host: string, unregisterPath: string = '/unregister', body?: string | FormData) {
         let data = '';
-        //if form data, else json
+        let contentType = Content_Type_Json_Data
+        //处理body， 如果是form data， 则格式化为 a=1&b=2这样的字符串
+        //如果本身体是json string， 则需要添加header， 里面有md5
         if (body && body instanceof FormData) {
             data = this.formDataString(unregisterPath, body);
+            contentType = Content_Type_Form_Data;
         }
-        if (this.config.authType === 'none') {
-            const msg = {
-                method: 'POST',
-                host: host,
-                headers: {
-                    'x-ca-websocket_api_type': 'UNREGISTER',
-                    'x-ca-seq': new Number(seqInc()).toString(),
-                    'x-ca-nonce': uuid.v4().toString(),
-                    'date': new Date().toUTCString(),
-                    'x-ca-timestamp': new Date().getTime().toString(),
-                    'isBase64': 0,
-                    'ca_version': '1',
-                },
-                path: unregisterPath,
-                body: data || body || '',
-            };
+
+        if (this.config.authType === 'none' || this.config.authType === 'appCode') {
+            const msg = this.createMsg('POST', host, unregisterPath, 'UNREGISTER', contentType, Accept_JSON, data);
             return msg;
         }
 
-        if (this.config.authType === 'appCode') {
-            const msg = {
-                method: 'POST',
-                host: host,
-                headers: {
-                    'x-ca-websocket_api_type': 'UNREGISTER',
-                    'x-ca-seq': new Number(seqInc()).toString(),
-                    'x-ca-nonce': uuid.v4().toString(),
-                    'date': new Date().toUTCString(),
-                    'x-ca-timestamp': new Date().getTime().toString(),
-                    'ca_version': '1',
-                    'isBase64': 0,
-                    'Authorization': `APPCODE ${this.config.appCode as string}`
-                },
-                path: unregisterPath,
-                body: data || body || '',
-            };
-            return msg;
-        }
+
         // else signature mode
         const util = new Util(this.config.appKey as string, this.config.appSecret as string, this.config.stage);
         const headers = util.createSignedRequestHeaders('POST', new URL(unregisterPath, this.config.url).toString(), {},
-            (body && body instanceof FormData) ? Content_Type_Form_Data : Content_Type_Json_Data, Content_Type_Json_Data, 'UNREGISTER', body)
+            contentType, Content_Type_Json_Data, 'UNREGISTER', body)
         const msg = {
             method: 'POST',
             host: host,
