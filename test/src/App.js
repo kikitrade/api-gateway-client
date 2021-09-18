c2a1f7150aa5bd8af23d495e2aef001060e6dfa9
holly.hao <hao.holly@gmail.com>
2021-09-18 21:06:44 +0800

    fix data

--- test/src/App.js
+++ test/src/App.js
@@ -1,34 +1,44 @@
 import './App.css';
-import { useEffect, useState } from 'react';
+import React from 'react';
 import { WS } from '@kikitrade/api-gateway-client';
 import env from 'react-dotenv'
+let i = 0;
+function messageList(d) {
+  return <p key={i++}>{JSON.stringify(d)}</p>
+}
 
-function App() {
-  const [messages, setMessages] = useState([]);
-  /**
-   * trim the data to 20
-   * @param {list} l 
-   * @returns 
-   */
-  function trim(l) {
-    if (l.length > 5) {
-      let excess = l.length - 5;
-      for (let i = 0; i < excess; i++) {
-        l.shift();
-      }
+/**
+ * trim the data to 20
+ * @param {list} l 
+ * @returns 
+ */
+function trim(l) {
+  if (l.length > 5) {
+    let excess = l.length - 5;
+    for (let i = 0; i < excess; i++) {
+      l.shift();
     }
-    return l;
   }
-  let listener = (data) => {
-    let d = [];
-    d.push(messages);
-    d.push(data);
-    d = trim(d);
-    setMessages(d)
-  };
+  return l;
+}
 
-  useEffect(() => {
-    console.log(env.url)
+class App extends React.Component {
+  state = {
+    messages: []
+  }
+
+
+  componentDidMount() {
+    let that = this;
+    this.listener = (data) => {
+      let d = [];
+      d.push(this.state.messages);
+      d.push(data);
+      d = trim(d);
+      that.setState({
+        messages: d
+      })
+    };
     let ws = new WS({
       url: env.url,
       authType: env.authType,
@@ -38,28 +48,28 @@ function App() {
       unregisterPath: "/api/unregister",
     });
 
-    ws.register(listener, "test111111222", {
+    ws.register(this.listener, "sdeedd", {
       room: "marketData",
       value: "",
     });
 
     setTimeout(() => {
-      ws.send("POST", "/api/room", "COMMON", { symbol: "BTC_USDT", room: "orderbook" });
+      ws.send("POST", "/api/room", "COMMON", { symbol: "BCH_USD", room: "orderbook" });
     }, 5000);
-  });
-
-  function messageList(d) {
-    return <p>{JSON.stringify(d)}</p>
   }
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p> messages: </p>
-        {messages.map(p => messageList(p))}
 
-      </header>
-    </div>
-  );
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <p> messages: </p>
+          {this.state.messages.map(p => messageList(p))}
+
+        </header>
+      </div>
+    );
+  }
+
 }
 
 export default App;
